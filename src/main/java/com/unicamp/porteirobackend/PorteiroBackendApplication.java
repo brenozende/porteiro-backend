451234d90af14393cf43a638d31aac1a448ce199
Breno Rezende Pinto <brenozende@gmail.com>
2023-04-04 17:41:13 -0300

    Primeiro commit

---
+++ src/main/java/com/unicamp/porteirobackend/PorteiroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unicamp.porteirobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorteiroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorteiroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicamp/porteirobackend/PorteiroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicamp.porteirobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorteiroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
