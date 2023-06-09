63262bb0c50507698567e0207c3737f14d06bc06
Breno Rezende Pinto <brenozende@gmail.com>
2023-06-09 12:25:40 -0300

    Removido endpoint inutilizado

--- src/main/java/com/unicamp/porteirobackend/controller/RegistrationController.java
+++
@@ -1,9 +0,0 @@
-package com.unicamp.porteirobackend.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/registration")
-public class RegistrationController {
-}
