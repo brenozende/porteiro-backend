a604869f4fa88de911e6d44dc7b3834c64e1c8da
Breno Rezende Pinto <brenozende@gmail.com>
2023-06-09 12:24:56 -0300

    Endpoints alterados para entrar na camada de segurança

--- src/main/java/com/unicamp/porteirobackend/controller/CommunicationsController.java
+++ src/main/java/com/unicamp/porteirobackend/controller/CommunicationsController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/communications")
+@RequestMapping("/api/communications")
 public class CommunicationsController {
 
     @Autowired
--- src/main/java/com/unicamp/porteirobackend/controller/ReservationController.java
+++ src/main/java/com/unicamp/porteirobackend/controller/ReservationController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/api/reservations")
 public class ReservationController {
 
     @Autowired
